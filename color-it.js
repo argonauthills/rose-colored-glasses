a651e66a66a4e427169025df81bcf775887838ab
Isaac Hess <isaachess@gmail.com>
2015-09-15 21:03:38 -0600

    Tab updates immediately when pressing buttons

--- color-it.js
+++ color-it.js
@@ -5,9 +5,18 @@ function refreshFilter() {
     console.log('refreshFilter')
     chrome.runtime.sendMessage({action:"getFilter"}, function(message) {
         var filter = message.payload.filter
-        console.log('filter', filter)
-        if (!!filter) document.body.setAttribute("style", "-webkit-filter:"+filter)
-        else document.body.setAttribute("style", "")
+        updateView(filter)
         console.log("got message", message)
     })
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log("colorit got message", request)
+    if (request.action == "updateView") updateView(request.payload.filter)
+})
+
+function updateView(filter) {
+    console.log('filter', filter)
+    if (!!filter) document.body.setAttribute("style", "-webkit-filter:"+filter)
+    else document.body.setAttribute("style", "")
 }
--- state.js
+++ state.js
@@ -7,8 +7,15 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action == "setFilter") {
         filter = request.payload.filter
         sendResponse(filterResponse(filter))
+        emitUpdateView(filter)
     }
 })
+
+function emitUpdateView(filter) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {action: 'updateView', payload: {filter: filter}})
+    })
+}
 
 function filterResponse(filter) {
     return { payload: {filter: filter} }
